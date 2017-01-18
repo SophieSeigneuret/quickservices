0f59bd545dcce90d2077fc95fa15a1de932e7fc9
sophieseigneuret <sophie.seigneuret@gmail.com>
2017-01-18 10:10:31 -0500

    modif page_head : branchement css

--- views/page_head.php
+++ views/page_head.php
@@ -3,5 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <title><?= NOM_SITE, ':',$page_title?></title>
+    <link rel="stylesheet" href="http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="icon" type="image/gif" href="" />
+
 </head>
 <body>
