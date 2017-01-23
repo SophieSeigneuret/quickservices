f40694c1d7d463f325acc4d35d1d44dcb2bd1713
Acosta <acosta@Acostas-MacBook-Pro.local>
2017-01-22 21:13:26 -0500

    etapes de l'inscription

---
+++ inscription_etape2.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'defines.php';
+$page_title = 'inscription etape 1';
+?>
+<?php
+require_once 'views/page_head.php';
+require_once 'views/header_inscription.php';
+?>
+<main>
+    <?php
+    require_once 'views/insc_fam_cand.php';
+    ?>
+
+</main>
+<?php
+require_once 'views/footer.php';
+require_once 'views/javascript.php';
+?>
+</body>
+</html>
+
+
---
+++ views/header_inscription.php
@@ -0,0 +1,15 @@
+<?php
+?>
+
+
+<header>
+    <!--    <h1>--><? //= $page_title ?><!--</h1>-->
+    <!--    --><?php //require('menu.php')?>
+    <div id="top">
+        <a href="index.php"><img id="logo" src="images/logo_provisoire.jpg" alt="logo quick services"/></a>
+        <div id="connexion">
+            <a href="formulaire_connexion.php">connexion</a>
+            <a href="inscription.php" >inscription</a>
+        </div>
+    </div>
+</header>
