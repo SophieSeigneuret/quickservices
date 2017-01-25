a0d33b27ebce3a4274a907d0216dd77836d79de1
Acosta <acosta@Acostas-MacBook-Pro.local>
2017-01-24 22:28:42 -0500

    mise a jour formulaire inscription

--- formulaire_inscription.php
+++ formulaire_inscription.php
@@ -7,7 +7,7 @@ require_once 'views/page_head.php';
 <?php
 //var_dump($_POST);
 
-$liste_horaire = array('Temps partiel', 'Temps plein', 'Occasionnel', 'Le matin', 'Après midi');
+$liste_horaire = array('Choisir...', 'Temps partiel', 'Temps plein', 'Occasionnel', 'Le matin', 'Après midi');
 
 
 function retire_accents($str) {
@@ -18,7 +18,11 @@ function retire_accents($str) {
 // Affichage initial du formulaire ? ou bien réception des données ?
 $en_reception = array_key_exists('saisie_nom', $_POST)
     && array_key_exists('saisie_prenom', $_POST)
-    && array_key_exists('saisie_email', $_POST);
+    && array_key_exists('saisie_email', $_POST)
+    && array_key_exists('saisie_telephone', $_POST)
+    && array_key_exists('saisie_adresse', $_POST)
+    && array_key_exists('saisie_cod_postal', $_POST)
+    && array_key_exists('saisie_cod_postal', $_POST);
 
 // Réception du nom
 $nom = '';
@@ -34,8 +38,7 @@ $prenom = '';
 $prenom_valide = true;
 if (array_key_exists('saisie_prenom', $_POST)) {
     $prenom = filter_input(INPUT_POST, 'saisie_prenom', FILTER_SANITIZE_STRING);
-    // 2 caractères minimum, le premier est un caractère majuscule
-    $prenom_valide = (1 === preg_match('/[A-Z]\w{1,}/', $prenom));
+    $prenom_valide = (1 === preg_match('/\w{1,}/', $prenom));
 }
 
 // Réception du courriel
@@ -45,37 +48,72 @@ if (array_key_exists('saisie_email', $_POST)) {
     $email = filter_input(INPUT_POST, 'saisie_email', FILTER_SANITIZE_STRING);
     $email_valide = (false !== filter_var($email, FILTER_VALIDATE_EMAIL));
 }
+//telephone
+$phone = '';
+$telephone_valide = true;
+if(array_key_exists('saisie_telephone', $_POST)){
+    $phone = filter_input(INPUT_POST, 'saisie_telephone' , FILTER_SANITIZE_STRING);
+    $telephone_valide = (1 === preg_match('/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/', $phone));//mayusculas y minusculas
+}
+
+//adresse
+$adresse = '';
+$adresse_valide = true;
+if(array_key_exists('saisie_adresse', $_POST)){
+    $adresse = filter_input(INPUT_POST, 'saisie_adresse' , FILTER_SANITIZE_STRING);
+    $adresse_valide = (1 === preg_match('/\w{1,}/', $adresse));//mayusculas y minusculas
+}
 
 // Code postal
 $cdpostal = '';
 $cdPostal_valide = true;
 if (array_key_exists('saisie_cod_postal', $_POST)) {
     $cdpostal = filter_input(INPUT_POST, 'saisie_cod_postal', FILTER_SANITIZE_STRING);
-    $cdPostal_valide = (false !== filter_var($cdpostal, FILTER_VALIDATE_EMAIL));
-}
-
-//password
-$password = '';
-$password_valide = true;
-if(array_key_exists('saisie_password', $_POST)){
-    $password = filter_input(INPUT_POST, 'saisie_password' , FILTER_SANITIZE_STRING);
-    $password_valide = (1 === preg_match('/^[a-zA-Z0-9_]{6}$/', $password));//mayusculas y minusculas
+    $cdPostal_valide = (1 === preg_match('/^[a-zA-Z]{1}[0-9]{1}[a-zA-Z]{1}(\-| |){1}[0-9]{1}[a-zA-Z]{1}[0-9]{1}$/', $cdpostal));
 }
 
 // Réception des horaires choisis
 $horaire_valides = true;
-$temps = array(); // Sports sélectionnés par l'utilisateur
+$temps = array(); // Horaire sélectionné par l'utilisateur
 if (array_key_exists('horaire', $_POST)) {
     $temps = $_POST['horaire'];
 }
-// Sports est valide si on est affichage initial ou bien si on a au moins un sport sélectionné
-if ($en_reception && empty($sports)) {
-    $sports_valides = false;
+// Horaire est valide si on est affichage initial ou bien si on a au moins un item sélectionné
+if ($en_reception && empty($temps)) {
+    $horaire_valides = false;
 }
 
-if ($en_reception && $nom_valide && $prenom_valide && $email_valide && $villes_valide && $sports_valides) {
+// Réception des dates calendrier choisis
+$calendrier_valide = true;
+$calendrier = array(); // Date sélectionnée par l'utilisateur
+if (array_key_exists('datepicker', $_POST)) {
+    $calendrier = $_POST['datepicker'];
+}
+// Date est valide si on est affichage initial ou bien si on a au moins un item sélectionné
+if ($en_reception && empty($calendrier)) {
+    $calendrier_valide = false;
+}
+
+// Réception des textarea
+$description_valide = true;
+$description = array();
+if (array_key_exists('saisie_description', $_POST)) {
+    $description = $_POST['saisie_description'];
+}
+if ($en_reception && empty($description)) {
+    $description_valide = false;
+}
+
+
+if ($en_reception && $nom_valide
+    && $prenom_valide
+    && $email_valide
+    && $telephone_valide
+    && $adresse_valide
+    && $cdPostal_valide
+    && $horaire_valides) {
     // Les données de formulaire sont valides
-    header('Location:ok.php');
+    header('Location:');
     exit;
 }
 
@@ -88,64 +126,62 @@ if ($en_reception && $nom_valide && $prenom_valide && $email_valide && $villes_v
 
         <!--Nom-->
         <!-- method GET par defaut, POST si précisé -->
-        <div class="<?= $nom_valide ? '' : 'invalid' ?>">
-            <label for="saisie_nom">Nom : </label>
-            <input type="text" placeholder="(entrez votre nom)" id="saisie_nom" name="saisie_nom"  value="<?= $nom ?>"/>
+        <div >
+            <label for="saisie_nom"><span>*</span> Nom : </label>
+            <input type="text" placeholder="votre nom" id="saisie_nom"
+                   name="saisie_nom" class="<?= $nom_valide ? '' : 'invalid' ?>" value="<?= $nom ?>"/>
             <?php if ( ! $nom_valide) { ?>
-                <p>Le nom doit contenir au moins un caractère.</p>
+                <p>Le nom n'est pas valide.</p>
             <?php } ?>
         </div>
-
-
-
-
-        <!--Nom-->
-        <!-- method GET par defaut, POST si précisé -->
-        <div class="<?= $nom_valide ? '' : 'invalid' ?>">
-            <label for="saisie_nom">Nom : </label>
-            <input type="text" placeholder="(entrez votre nom)" id="saisie_nom" name="saisie_nom"  value="<?= $nom ?>"/>
-            <?php if ( ! $nom_valide) { ?>
-                <p>Le nom doit contenir au moins un caractère.</p>
-            <?php } ?>
-        </div>
-
-
-
-
 
         <!--prenom-->
-        <div class="<?= $prenom_valide ? '' : 'invalid' ?>">
-            <label for="saisie_prenom">Prénom : </label>
-            <input type="text" placeholder="(entrez votre prénom)" id="saisie_prenom" name="saisie_prenom" value="<?= $prenom ?>"/>
+        <div >
+            <label for="saisie_prenom"><span>*</span> Prénom : </label>
+            <input type="text" placeholder="votre prénom" id="saisie_prenom"
+                   name="saisie_prenom" class="<?= $prenom_valide ? '' : 'invalid' ?>" value="<?= $prenom ?>"/>
             <?php if ( ! $prenom_valide) { ?>
-                <p>Le prénom doit contenir 2 caractères minimum, le premier étant un caractère majuscule.</p>
+                <p>Le prenom n'est pas valide</p>
             <?php } ?>
         </div>
 
         <!--courriel-->
-        <div class="<?= $email_valide ? '' : 'invalid' ?>">
-            <label for="saisie_email">Courriel : </label>
-            <input type="text" placeholder="(entrez votre courriel)" id="saisie_email" name="saisie_email" value="<?= $email ?>"/>
+        <div >
+            <label for="saisie_email"><span>*</span> Courriel : </label>
+            <input type="text" placeholder=" exemple@domaine.com" id="saisie_email"
+                   name="saisie_email" class="<?= $email_valide ? '' : 'invalid' ?>" value="<?= $email ?>"/>
             <?php if ( ! $email_valide) { ?>
                 <p>L'adresse courriel n'est pas valide.</p>
             <?php } ?>
         </div>
 
-        <!--code postal-->
-        <div class="<?= $cdPostal_valide ? '' : 'invalid' ?>">
-            <label for="saisie_cod_postal">Code postal : </label>
-            <input type="text" placeholder="(entrez votre code postal)" id="saisie_cod_postal" name="saisie_cod_postal" value="<?= $cdpostal ?>"/>
-            <?php if ( ! $cdPostal_valide) { ?>
-                <p>Le code postal n'est pas valide.</p>
+        <!--    telephone -->
+        <div >
+            <label for="saisie_telephone"><span>*</span> Téléphone : </label>
+            <input type="tel" placeholder="(000) 000-0000" id="saisie_telephone"
+                   name="saisie_telephone" class="<?=$telephone_valide  ? '' : 'invalid'?>" value="<?=$phone?>"/>
+
+            <?php if(!$telephone_valide){ ?>
+                <p>Le numero téléphone n'est pas valide.</p>
             <?php } ?>
         </div>
 
-        <!--    password -->
-        <div class="<?=$password_valide  ? '' : 'invalid'?>">
-            <label for="saisie_password">Password : </label>
-            <input type="password" placeholder="(entrez votre password)" id="saisie_password" name="saisie_password" value="<?=$password?>"/>
+        <!--Adresse-->
+        <div >
+            <label for="saisie_adresse"><span>*</span> Adresse : </label>
+            <input type="text" placeholder="votre adresse" id="saisie_adresse"
+                   name="saisie_adresse" class="<?= $adresse_valide ? '' : 'invalid' ?>" value="<?= $adresse ?>"/>
+            <?php if ( ! $adresse_valide) { ?>
+                <p>L'adresse n'est pas valide.</p>
+            <?php } ?>
+        </div>
 
-            <?php if(!$password_valide){ ?>
+        <!--code postal-->
+        <div>
+            <label for="saisie_cod_postal"><span>*</span> Code postal : </label>
+            <input type="text" placeholder="h1t 1m4 / H1T 1M4" id="saisie_cod_postal"
+                   name="saisie_cod_postal" class="<?= $cdPostal_valide ? '' : 'invalid' ?>" value="<?= $cdpostal ?>"/>
+            <?php if ( ! $cdPostal_valide) { ?>
                 <p>Le code postal n'est pas valide.</p>
             <?php } ?>
         </div>
@@ -153,9 +189,9 @@ if ($en_reception && $nom_valide && $prenom_valide && $email_valide && $villes_v
 
         <!--    horaire -->
         <!-- Select liste de horaire -->
-        <div class="<?= $horaire_valides ? '' : 'invalid' ?>">
+        <div >
             <label for="horaire">Horaire : </label>
-            <select name="horaire[]" id="horaire" multiple="multiple">
+            <select class="<?= $horaire_valides ? '' : 'invalid' ?>" name="horaire[]" id="horaire" ><!-- multiple="multiple"-->
                 <?php foreach ($liste_horaire as $temps) {
                     $option_value = retire_accents($temps);
                     ?>
@@ -165,71 +201,39 @@ if ($en_reception && $nom_valide && $prenom_valide && $email_valide && $villes_v
                 <?php } ?>
             </select>
             <?php if ( ! $horaire_valides) { ?>
-                <p>Au moins un sport doit être sélectionné.</p>
+                <p>Au moins un horaire doit être sélectionné.</p>
             <?php } ?>
         </div>
 
-        <!--    calendrie -->
+        <!--    calendrier -->
 
-        <label for="datepicker">Date </label>
-        <input type="text" name="date_debut" id="datepicker" value=""/>
-
+        <div>
+            <label for="datepicker"><span>*</span> Date : </label>
+            <input type="date" name="datepicker" id="datepicker"
+                   class="calendrier <?= $calendrier_valide ? '' : 'invalid' ?>" value="<?= $calendrier ?>"/>
+            <?php if ( ! $calendrier_valide) { ?>
+                <p>Une date doit être sélectionnée.</p>
+            <?php } ?>
+        </div>
 
 
         <!--    Description d l'emploi -->
-        <div class="">
-            <label for="date">Description de l'emploi</label>
-            <textarea name="textarea" rows="5" cols="68">
-            Vous pouvez écrire quelque chose ici.
-            </textarea>
-
+        <div>
+            <label for="saisie_description">Description de l'emploi</label>
+            <textarea name="textarea" placeholder="Description..." id="saisie_description"
+                      class="<?= $description_valide ? '' : 'invalid' ?>" rows="5" cols="68" ></textarea>
+            <?php if ( ! $description_valide) { ?>
+                <p>Vous devez faire une description</p>
+            <?php } ?>
         </div>
 
         <div>
-            <input type="submit" value="Soumettre"/>
+            <input type="submit" value="Inscription"/>
         </div>
     </form>
     <p><a href="formulaire_connexion.php">J'ai déjà un compte</a></p>
-    <p>Pas de compte ? <a href="inscription.php">Inscrivez-vous !</a></p>
+    <p><span>Pas de compte ? </span><a href="inscription.php">Inscrivez-vous !</a></p>
 </div>
 
-<script>
-    /* DATEPICKER */
-    // ajouter 1 jour au datepicker début et fin de saison
-    var min_date = new Date(forfait_voulu.debut_saison);
-    min_date.setDate(min_date.getDate()+1);
-    var max_date = new Date(forfait_voulu.fin_saison);
-    max_date.setDate(max_date.getDate()+1);
-    // rajout du datepicker jQuery
-    $("#datepicker").datepicker({
-        showOn: 'button',
-        buttonImage: 'images/calendar_icon.gif',
-        numberOfMonths: 2,
-        showButtonPanel: true,
-        dateFormat: "dd-mm-yy",
-        minDate: min_date,
-        maxDate: max_date
-    });
 
-    /* VALIDER DATEPICKER */
-    var champ_datepicker = $("#datepicker");
-    var img_calendrier = $("#resa_client button");
-    if (champ_datepicker.val().trim().length < MIN_NB_CAR) {
-        champ_datepicker.addClass("error");  // ajoute la classe error
-        if (!champ_datepicker.next().is(".error_msg")) {
-            img_calendrier.after("<p class='error_msg'>Champ obligatoire</p>");  // ajoute un paragraphe de message apres l'element input
-        }
-        formulaire_valide = false;
-    } else {
-        champ_datepicker.removeClass("error");
-        img_calendrier.next(".error_msg").remove();
-    }
-
-
-
-</script>
-<?php
-
-//require_once 'views/javascript.php';
-?>
 </html>
