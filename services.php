df0bafec012eea9251cbb096bdb411ec17183e34
Acosta <acosta@Acostas-MacBook-Pro.local>
2017-01-28 16:45:58 -0500

    branchement bd

--- services.php
+++ services.php
@@ -1,6 +1,9 @@
 <?php
 // $page_title = 'soutien_scolaire';
+
 require_once 'defines.php';
+require_once 'db/db_access.php';
+
 require_once 'views/page_head.php';
 require_once 'views/header.php';
 ?>
